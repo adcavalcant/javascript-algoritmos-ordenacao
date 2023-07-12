f6eea364f057419aa578b906efd1d7799e7ee6e3
adcavalcant <aderc19@gmail.com>
2023-07-12 14:52:53 -0300

    Exibindo os livros

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //ALGORITMO QUE PEGA O LIVRO MAIS BARATO DENTRO DO ARRAY.
 
-//                   0   1   2   3   4   5
-const precosLivro = [25, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
 let maisBarato = 0;
 
-for (let atual = 0; atual <= precosLivro.length; atual++) {
-  if (precosLivro[atual] < precosLivro[maisBarato]) {
-    maisBarato = atual
+for (let atual = 0; atual < livros.length; atual++) {
+  if (livros[atual].preco < livros[maisBarato].preco) {
+    maisBarato = atual;
   }
 }
 
-console.log(`O livro mais barato custa ${precosLivro[maisBarato]}`);
+console.log(
+  `O livro mais barato custa ${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`
+);
